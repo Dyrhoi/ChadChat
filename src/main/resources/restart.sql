cbed9dfa550300b29d376e7aadd039768ed08d1b
Dyrhoi <realarcoz@gmail.com>
2020-09-30 14:19:05 +0200

    Start Script Fixed.

--- src/main/resources/restart.sql
+++ src/main/resources/restart.sql
@@ -6,10 +6,11 @@ create user 'chadchat'@'localhost';
 
 grant all privileges on chadchat.* to 'chadchat'@'localhost';
 
+use chadchat;
 create table users(
     id int PRIMARY KEY AUTO_INCREMENT,
-    username string(25) NOT NULL UNIQUE,
+    username varchar(25) NOT NULL UNIQUE,
     salt BINARY(16) NOT NULL,
     secret BINARY(32) NOT NULL,
-    _date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-)
+    _date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
